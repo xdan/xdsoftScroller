305c80858ab828eca3a126b03cd809807df9bdbb
Valeriy <skoder@ya.ru>
2014-10-27 14:44:40 +0600

    Version 1.0.1

--- jquery.xdsoftScroller.js
+++ jquery.xdsoftScroller.js
@@ -1,107 +1,165 @@
-$.fn.xdsoftScroller = function( _percent ) {
-	return this.each(function() {
-		var timeboxparent = $(this);
-		if( !$(this).hasClass('xdsoft_scroller_box') ) {
-			var pointerEventToXY = function( e ) {
-					var out = {x:0, y:0};
-					if( e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel' ) {
-						var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-						out.x = touch.pageX;
-						out.y = touch.pageY;
-					}else if (e.type == 'mousedown' || e.type == 'mouseup' || e.type == 'mousemove' || e.type == 'mouseover'|| e.type=='mouseout' || e.type=='mouseenter' || e.type=='mouseleave') {
-						out.x = e.pageX;
-						out.y = e.pageY;
+/**
+ * @preserve jQuery Scroller plugin v1.0.1
+ * @homepage http://xdsoft.net
+ * (c) 2014, Chupurnov Valeriy.
+ */
+/*global document,window,jQuery,setTimeout,clearTimeout,console*/
+(function ($) {
+	'use strict';
+	$.fn.xdsoftScroller = function (percent) {
+		return this.each(function () {
+			var timeboxparent = $(this),
+				pointerEventToXY = function (e) {
+					var out = {x: 0, y: 0},
+						touch;
+					if (e.type === 'touchstart' || e.type === 'touchmove' || e.type === 'touchend' || e.type === 'touchcancel') {
+						touch  = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+						out.x = touch.clientX;
+						out.y = touch.clientY;
+					} else if (e.type === 'mousedown' || e.type === 'mouseup' || e.type === 'mousemove' || e.type === 'mouseover' || e.type === 'mouseout' || e.type === 'mouseenter' || e.type === 'mouseleave') {
+						out.x = e.clientX;
+						out.y = e.clientY;
 					}
 					return out;
 				},
 				move = 0,
-				timebox = timeboxparent.children().eq(0),
-				parentHeight = timeboxparent[0].clientHeight,
-				height = timebox[0].offsetHeight,
-				scrollbar = $('<div class="xdsoft_scrollbar"></div>'),
-				scroller = $('<div class="xdsoft_scroller"></div>'),
+				timebox,
+				parentHeight,
+				height,
+				scrollbar,
+				scroller,
 				maximumOffset = 100,
-				start = false;
+				start = false,
+				startY = 0,
+				startTop = 0,
+				h1 = 0,
+				touchStart = false,
+				startTopScroll = 0,
+				calcOffset = function () {};
+			if (percent === 'hide') {
+				timeboxparent.find('.xdsoft_scrollbar').hide();
+				return;
+			}
+			if (!$(this).hasClass('xdsoft_scroller_box')) {
+				timebox = timeboxparent.children().eq(0);
+				parentHeight = timeboxparent[0].clientHeight;
+				height = timebox[0].offsetHeight;
+				scrollbar = $('<div class="xdsoft_scrollbar"></div>');
+				scroller = $('<div class="xdsoft_scroller"></div>');
+				scrollbar.append(scroller);
 
-			scrollbar.append(scroller);
-
-			timeboxparent.addClass('xdsoft_scroller_box').append(scrollbar);
-			scroller.on('mousedown.xdsoft_scroller',function ( event ) {
-				if( !parentHeight )
-					timeboxparent.trigger('resize_scroll.xdsoft_scroller',[_percent]);
-				var pageY = event.pageY,
-					top = parseInt(scroller.css('margin-top')),
-					h1 = scrollbar[0].offsetHeight;
-				$(document.body).addClass('xdsoft_noselect');
-				$([document.body,window]).on('mouseup.xdsoft_scroller',function arguments_callee() {
-					$([document.body,window]).off('mouseup.xdsoft_scroller',arguments_callee)
-						.off('mousemove.xdsoft_scroller',move)
-						.removeClass('xdsoft_noselect');
-				});
-				$(document.body).on('mousemove.xdsoft_scroller',move = function(event) {
-					var offset = event.pageY-pageY+top;
-					if( offset<0 )
+				timeboxparent.addClass('xdsoft_scroller_box').append(scrollbar);
+				calcOffset = function calcOffset(event) {
+					var offset = pointerEventToXY(event).y - startY + startTopScroll;
+					if (offset < 0) {
 						offset = 0;
-					if( offset+scroller[0].offsetHeight>h1 )
-						offset = h1-scroller[0].offsetHeight;
-					timeboxparent.trigger('scroll_element.xdsoft_scroller',[maximumOffset?offset/maximumOffset:0]);
-				});
-			});
-
-			timeboxparent
-				.off('scroll_element.xdsoft_scroller')
-				.off('resize_scroll.xdsoft_scroller')
-				.on('scroll_element.xdsoft_scroller',function( event,percent ) {
-					if( !parentHeight )
-						timeboxparent.trigger('resize_scroll.xdsoft_scroller',[percent,true]);
-					percent = percent>1?1:(percent<0||isNaN(percent))?0:percent;
-					scroller.css('margin-top',maximumOffset*percent);
-					timebox.css('marginTop',-parseInt((height-parentHeight)*percent))
-				})
-				.on('resize_scroll.xdsoft_scroller',function( event,_percent,noTriggerScroll ) {
-					parentHeight = timeboxparent[0].clientHeight;
-					height = timebox[0].offsetHeight;
-					var percent = parentHeight/height,
-						sh = percent*scrollbar[0].offsetHeight;
-					if( percent>1 )
-						scroller.hide();
-					else{
-						scroller.show();
-						scroller.css('height',parseInt(sh>10?sh:10));
-						maximumOffset = scrollbar[0].offsetHeight-scroller[0].offsetHeight;
-						if( noTriggerScroll!==true )
-							timeboxparent.trigger('scroll_element.xdsoft_scroller',[_percent?_percent:Math.abs(parseInt(timebox.css('marginTop')))/(height-parentHeight)]);
 					}
-				});
-		
-				timeboxparent.off('mousewheel');
-				timeboxparent.on('mousewheel', function(event) {
-					var top = Math.abs(parseInt(timebox.css('marginTop')));
-					timeboxparent.trigger('scroll_element.xdsoft_scroller',[(top-event.deltaY*20)/(height-parentHeight)]);
+					if (offset + scroller[0].offsetHeight > h1) {
+						offset = h1 - scroller[0].offsetHeight;
+					}
+					timeboxparent.trigger('scroll_element.xdsoft_scroller', [maximumOffset ? offset / maximumOffset : 0]);
+				};
+
+				scroller
+					.on('touchstart.xdsoft_scroller mousedown.xdsoft_scroller', function (event) {
+						if (!parentHeight) {
+							timeboxparent.trigger('resize_scroll.xdsoft_scroller', [percent]);
+						}
+
+						startY = pointerEventToXY(event).y;
+						startTopScroll = parseInt(scroller.css('margin-top'), 10);
+						h1 = scrollbar[0].offsetHeight;
+
+						if (event.type === 'mousedown') {
+							if (document) {
+								$(document.body).addClass('xdsoft_noselect');
+							}
+							$([document.body, window]).on('mouseup.xdsoft_scroller', function arguments_callee() {
+								$([document.body, window]).off('mouseup.xdsoft_scroller', arguments_callee)
+									.off('mousemove.xdsoft_scroller', calcOffset)
+									.removeClass('xdsoft_noselect');
+							});
+							$(document.body).on('mousemove.xdsoft_scroller', calcOffset);
+						} else {
+							touchStart = true;
+						}
+						event.stopPropagation();
+						event.preventDefault();
+					})
+					.on('touchmove', function (event) {
+						if (touchStart) {
+							event.preventDefault();
+							calcOffset(event);
+						}
+					})
+					.on('touchend touchcancel', function (event) {
+						touchStart =  false;
+						startTopScroll = 0;
+					});
+
+				timeboxparent
+					.on('scroll_element.xdsoft_scroller', function (event, percentage) {
+						if (!parentHeight) {
+							timeboxparent.trigger('resize_scroll.xdsoft_scroller', [percentage, true]);
+						}
+						percentage = percentage > 1 ? 1 : (percentage < 0 || isNaN(percentage)) ? 0 : percentage;
+
+						scroller.css('margin-top', maximumOffset * percentage);
+
+						setTimeout(function () {
+							timebox.css('marginTop', -parseInt((timebox[0].offsetHeight - parentHeight) * percentage, 10));
+						}, 10);
+					})
+					.on('resize_scroll.xdsoft_scroller', function (event, percentage, noTriggerScroll) {
+						var percent, sh;
+						parentHeight = timeboxparent[0].clientHeight;
+						height = timebox[0].offsetHeight;
+						percent = parentHeight / height;
+						sh = percent * scrollbar[0].offsetHeight;
+						if (percent >= 1) {
+							scroller.hide();
+						} else {
+							scroller.show();
+							scroller.css('height', parseInt(sh > 10 ? sh : 10, 10));
+							maximumOffset = scrollbar[0].offsetHeight - scroller[0].offsetHeight;
+							if (noTriggerScroll !== true) {
+								timeboxparent.trigger('scroll_element.xdsoft_scroller', [percentage || Math.abs(parseInt(timebox.css('marginTop'), 10)) / (height - parentHeight)]);
+							}
+						}
+					});
+
+				timeboxparent.on('mousewheel', function (event) {
+					var top = Math.abs(parseInt(timebox.css('marginTop'), 10));
+
+					top = top - (event.deltaY * 20);
+					if (top < 0) {
+						top = 0;
+					}
+
+					timeboxparent.trigger('scroll_element.xdsoft_scroller', [top / (height - parentHeight)]);
 					event.stopPropagation();
 					return false;
 				});
-				
-	
-			timeboxparent.off('touchstart');
-			timeboxparent.on('touchstart',function( event ) {
-				start = pointerEventToXY(event);
-			});
-			timeboxparent.off('touchmove');
-			timeboxparent.on('touchmove',function( event ) {
-				if( start ) {
-					var coord = pointerEventToXY(event), top = Math.abs(parseInt(timebox.css('marginTop')));
-					timeboxparent.trigger('scroll_element.xdsoft_scroller',[(top-(coord.y-start.y))/(height-parentHeight)]);
-					event.stopPropagation();
-					event.preventDefault();
+
+				timeboxparent.on('touchstart', function (event) {
 					start = pointerEventToXY(event);
-				}
-			});
-			timeboxparent.off('touchend touchcancel');
-			timeboxparent.on('touchend touchcancel',function( event ) {
-				start = false;
-			});
-		}
-		timeboxparent.trigger('resize_scroll.xdsoft_scroller',[_percent]);
-	});
-};
+					startTop = Math.abs(parseInt(timebox.css('marginTop'), 10));
+				});
+
+				timeboxparent.on('touchmove', function (event) {
+					if (start) {
+						event.preventDefault();
+						var coord = pointerEventToXY(event);
+						timeboxparent.trigger('scroll_element.xdsoft_scroller', [(startTop - (coord.y - start.y)) / (height - parentHeight)]);
+					}
+				});
+
+				timeboxparent.on('touchend touchcancel', function (event) {
+					start = false;
+					startTop = 0;
+				});
+			}
+			timeboxparent.trigger('resize_scroll.xdsoft_scroller', [percent]);
+		});
+	};
+}(jQuery));
